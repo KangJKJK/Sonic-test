9e43093b57afbd32db5b5a4b84ac62b5da68072c
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-01 20:51:41 +0900

    Update sonic.js

--- sonic.js
+++ sonic.js
@@ -46,7 +46,8 @@ function getKeypairFromPrivateKey(privateKey) {
 const sendTransaction = (transaction, keyPair) => new Promise(async (resolve) => {
     try {
         transaction.feePayer = keyPair.publicKey; // 수수료 지불자 설정
-        await transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash; // recentBlockhash 설정
+        const recentBlockhash = await connection.getRecentBlockhash(); // recentBlockhash 가져오기
+        transaction.recentBlockhash = recentBlockhash.blockhash; // recentBlockhash 설정
 
         transaction.partialSign(keyPair);
         const rawTransaction = transaction.serialize();
@@ -59,6 +60,7 @@ const sendTransaction = (transaction, keyPair) => new Promise(async (resolve) =>
         resolve(error);
     }
 });
+
 
 // 지연 시간 함수
 const delay = () => {
